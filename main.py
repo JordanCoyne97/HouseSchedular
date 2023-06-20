4ff1466629d4e34c68d212a23f2e0c42b6da06df
Jordan, Coyne <jordancoyne@hotmail.com>
2023-06-20 23:29:03 +0100

    working bs

--- main.py
+++ main.py
@@ -1,10 +1,30 @@
 import smtplib
 import ssl
+from bs4 import BeautifulSoup
+import requests
+
+
+def web_scraper():
+    url1 = "https://www.daft.ie/property-for-rent/galway-city?sort=publishDateDesc&from=0&pageSize=20"
+    url2 = "https://www.daft.ie/property-for-rent/galway-city?sort=publishDateDesc&pageSize=20&from=20"
+    url3 = "https://www.daft.ie/property-for-rent/galway-city?sort=publishDateDesc&pageSize=20&from=40"
+    url4 = "https://www.daft.ie/property-for-rent/galway-city?sort=publishDateDesc&pageSize=20&from=60"
+    url5 = "https://www.daft.ie/property-for-rent/galway-city?sort=publishDateDesc&pageSize=20&from=80"
+
+    urls = [url1, url2, url3, url4, url5]
+
+    for url in urls:
+        source = requests.get(url)
+        soup = BeautifulSoup(source.content, 'html.parser')
+
+        # TODO scrape list of house ids
+        print("test")
 
 
 def start_schedule():
     print("Starting schedule")
-    send_email()
+    #send_email()
+    web_scraper()
 
 
 def send_email():
